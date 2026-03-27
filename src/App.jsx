a956d7c528fd4f10448fdf3c636af9092c4d67c3
Narcisse <Jean45381@gmail.com>
2026-03-27 12:57:27 -0400

    Mise à jour EmployeeForm et EmployeeList

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import EmployeeForm from"./Components/EmployeeForm";
-import EmployeeList from"./Components/EmployeeList";
-import EmployeeCard from './Components/EmployeeCard';
-function App() {
-  const [employees,setEmployees]= useState([
-    {name:"Jen",position:"Dev",department:"It",status:"Actif"},
-    {name:"Bob",position:"RH",department:"HR",status:"Actif"}
-  ]);
-  console.log(employees)
-  function addEmployee(newEmp){
-    setEmployees([...employees, newEmp]);
-  }
-  return(<>
-  <div>
+import { useState } from "react";
+import EmployeeForm from "./composants/EmployeeForm";
+import EmployeeList from "./composants/EmployeeList";
+import employees from "./data/employees";
 
-    <h1>hello </h1>
-  <EmployeeForm addEmployee={addEmployee}/>
-  <EmployeeList employees={employees}/>
-  <EmployeeCard/>
-</div>
-    </>
-  )
+function App() {
+  const [employees, setEmployees] = useState(initialEmployees);
+
+  const addEmployee = (newEmployee) => {
+    setEmployees([...employees, newEmployee]);
+  };
+
+  return (
+    <div>
+      <h1>Liste des employés</h1>
+      <EmployeeForm addEmployee={addEmployee} />
+      <EmployeeList employees={employees} />
+    </div>
+  );
 }
 
-export default App
+export default App;
